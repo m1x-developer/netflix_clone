eed92d70c46bd6e3bb05f7380364635cdad87a84
Korolyov Maks <m.korolev@ddplanet.ru>
2022-07-19 21:50:43 +0300

    test

--- src/utils/youtube_parse_url.ts
+++
@@ -1,5 +0,0 @@
-export function youtube_parse_url(url: string) {
-    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-    const match = url.match(regExp);
-    return (match && match[7].length === 11) ? match[7] : false;
-}
